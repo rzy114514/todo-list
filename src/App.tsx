9f6385fb4b63cd07ad2bf22698e6cfbca8058a81
rzy <rzy1145141919810@outlook.com>
2025-08-29 19:48:15 +0800

    style:修改 App.ts 的全局布局 gap 和 padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Input from "./components/Input";
 const App:React.FC = () => {
   return (
-  <div w-screen h-screen p-4 un-flex="~ col items-center" gap-5>
-    
+  <div w-screen h-screen p-8 un-flex="~ col items-center" gap-5>
+    <Input></Input>
+    aaaa
   </div>);
 };
 
