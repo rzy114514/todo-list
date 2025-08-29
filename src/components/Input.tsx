079def4e8d1c3330c603f72a47f8ed74b3b05540
rzy <rzy1145141919810@outlook.com>
2025-08-29 20:18:40 +0800

    style:添加 max-w 样式

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Input:React.FC<Props> = ({}) => {
   return (
-    <div un-flex="~ justify-center" un-w="65%" h-14 rd-7 un-shadow="sm gray">
+    <div un-flex="~ justify-center" un-w="65%" h-14 rd-7 un-shadow="sm gray" max-w-125>
       <input type="text" placeholder='请输入文本' un-w="90%" text-lg outline-none/>
     </div>
   )
