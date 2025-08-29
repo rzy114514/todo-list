ee6322a37d4b66d7f38998b7928575192928aa9c
rzy <rzy1145141919810@outlook.com>
2025-08-29 22:42:42 +0800

    feat:List.tsx 实现完整功能 并去除无用 Props 结构

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
+import useStore from "../stores/todo";
 
-type Props = {};
-
-const List: React.FC<Props> = ({}) => {
+const List: React.FC = () => {
+  const { items } = useStore();
   return (
-    <div un-w="65%" h-14 rd-7 un-shadow="sm gray" max-w-125>
-      <Item itemID={114514}></Item>
+    <div un-w="65%" rd-7 un-shadow="sm gray" max-w-125>
+      {items.map(({ id }) => (
+        <Item itemID={id} key={id}></Item>
+      ))}
     </div>
   );
 };
@@ -19,18 +21,58 @@ type Props2 = {
 const Item: React.FC<Props2> = ({ itemID }) => {
   // 编辑状态
   const [isEditing, setisEditing] = useState(false);
+  const { items, remove, toggle, setContext } = useStore();
+  const initDone = useStore
+    .getState()
+    .items.find((item) => itemID === item.id)?.done;
+  const [value, setValue] = useState(
+    () => items.find(({ id }) => id === itemID)?.context ?? ""
+  );
+
+  function onKeyUp(e: React.KeyboardEvent) {
+    if(e.key === "Enter") setContext(itemID, value)
+    else if(e.key !== "Escape") return
+    setisEditing(false)
+  }
+
   return (
-    <div w-full h-14 un-border="b gray" last:border-none un-flex="~ justify-around items-center">
+    <div
+      w-full
+      h-14
+      un-border="b gray"
+      last:border-none
+      un-flex="~ justify-around items-center"
+    >
       {isEditing ? (
-        <input type="text" value={"test"}  un-w="60%" 
-        outline-none
-         />
+        <input
+          type="text"
+          value={value}
+          un-w="60%"
+          outline-none
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setValue(e.target.value)
+          }
+          onKeyUp={onKeyUp}
+        />
       ) : (
-        <p onClick={() => setisEditing(true)} un-w="60%">{"test"}</p>
+        <p onClick={() => setisEditing(true)} un-w="60%">
+          {value}
+        </p>
       )}
       <div gap-5 un-flex="~">
-        <input type="checkbox" />
-      <a font-bold un-text="red-200 hover:red-400" text-lg>删除</a>
+        <input
+          type="checkbox"
+          checked={initDone}
+          onChange={() => toggle(itemID)}
+        />
+        <a
+          font-bold
+          un-text="red-200 hover:red-400"
+          text-lg
+          onClick={() => remove(itemID)}
+        >
+          删除
+        </a>
       </div>
     </div>
   );
