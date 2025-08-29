17a63a982a4c335eccc588e1356ebbc2a26a0ee1
rzy <rzy1145141919810@outlook.com>
2025-08-29 19:50:20 +0800

    feat:添加 Input.tsx 组件

---
+++ src/components/Input.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Input:React.FC<Props> = ({}) => {
+  return (
+    <div un-flex="~ justify-center" un-w="65%" h-14 rd-7 un-shadow="sm gray">
+      <input type="text" placeholder='请输入文本' un-w="90%" text-lg outline-none/>
+    </div>
+  )
+}
+
+export default Input
